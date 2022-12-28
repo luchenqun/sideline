d2a5f1ff51cc0e68a1ff512326d7626d91c8ffc4
helloworld <helloworld@qq.com>
2022-12-28 21:22:10 +0800

    update RegistEmployer

--- x/sideline/keeper/msg_server_regist_employer.go
+++ x/sideline/keeper/msg_server_regist_employer.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"errors"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"sideline/x/sideline/types"
@@ -10,8 +11,24 @@ import (
 func (k msgServer) RegistEmployer(goCtx context.Context, msg *types.MsgRegistEmployer) (*types.MsgRegistEmployerResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	_, found := k.GetEmployer(ctx, msg.Creator)
+	if found {
+		return &types.MsgRegistEmployerResponse{}, errors.New("you have regist")
+	}
+
+	employer := types.Employer{
+		Index:     msg.Creator, // 以用户地址为map的索引
+		Name:      msg.Name,
+		Introduce: msg.Introduce,
+		Email:     msg.Email,
+		Avatar:    msg.Avatar,
+		Address:   msg.Creator,
+		TaskCount: 0,
+		TaskFail:  0,
+		Feedbacks: nil,
+	}
+
+	k.SetEmployer(ctx, employer)
 
 	return &types.MsgRegistEmployerResponse{}, nil
 }
